caad2059b4287adf6b29bd8e26f8cb519d7deec7
Faithful Austin - Ilagha <missfair42@gmail.com>
2023-01-25 22:32:14 +0100

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Info from "./Components/Info";
 import About from "./Components/About";
 import Interest from "./Components/Interest";
+import Footer from "./Components/Footer";
 import "./index.css"
 function App(){
 return(
@@ -8,6 +9,7 @@ return(
         <Info/>
         <About/>
         <Interest/>
+        <Footer/>
     </div>
 )
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,14 @@
 function Footer(){
 
+    return(
+        <div className="footer"> 
+        <div className="iconCont"><i class="bi bi-twitter"></i></div>
+        <div className="iconCont"><i class="bi bi-facebook"></i></div>
+        <div className="iconCont"><i class="bi bi-instagram"></i></div>
+        <div className="iconCont"><i class="bi bi-github"></i></div>
+        </div>
+    )
+
+  
 }
 export default Footer;
